dc8e0e26047ac2546b01de5035157769dded11e7
Amruth L P <134767358+AmruthLP12@users.noreply.github.com>
2024-10-01 20:10:37 +0530

    RightSideBar-article started

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
 
-  const loggedIn = {firstName : 'Amruth'}
+  const loggedIn = {firstName : 'Amruth', lastName :'L P' , email :'amruth@gmail.com'}
 
   return (
     <section className="home">
@@ -23,7 +24,14 @@ const Home = () => {
           totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+      user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance:123.50},{currentBalance:500.56}]}
+      />
     </section>
   );
 };
